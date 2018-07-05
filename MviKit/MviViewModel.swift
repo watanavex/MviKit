06289f167d52e2f7432fe05f4904c35b6d963aa0
watanave <susan.se3p@gmail.com>
2018-07-05 19:31:41 +0900

    Add ViewModel base class

--- MviKit/MviViewModel.swift
+++ MviKit/MviViewModel.swift
@@ -19,6 +19,84 @@ public protocol MviViewModelProtocol {
     func process(intents: Observable<Intent>)
 }
 
+public class MviViewModel<I, S, T, A, R, RR, DR>: MviViewModelProtocol where I: MviIntent, S: MviState, A: MviAction, R: MviResult, RR: MviRetentionResult, DR: MviDisposableResult {
+    public typealias Intent = I
+    public typealias State = S
+    public typealias Task = T
+    public typealias Action = A
+    public typealias Result = R
+    public typealias RetentionResult = RR
+    public typealias DisposableResult = DR
+    
+    private let intentsSubject = PublishSubject<Intent>()
+    let processor: AnyProcessor<Action, Result>
+    let disposeBag = DisposeBag()
+    
+    private lazy var result: Observable<Result> = {
+        return self.resultObservable(intentsSubject: self.intentsSubject)
+    }()
+    public lazy var state: Observable<State> = {
+        return self.stateObservable(resultObservable: self.result)
+    }()
+    public lazy var task: Observable<Task> = {
+        return self.taskObservable(resultObservable: self.result)
+    }()
+    
+    private func resultObservable(intentsSubject: PublishSubject<Intent>) -> Observable<Result> {
+        let connectable = intentsSubject
+            .compose(self.intentFilter())
+            .map(self.actionFrom)
+            .flatMap(self.processor.process)
+            .publish()
+        connectable.connect().disposed(by: self.disposeBag)
+        return connectable
+    }
+    private func stateObservable(resultObservable: Observable<Result>) -> Observable<State> {
+        let connectable = resultObservable.filter { $0 is RetentionResult }
+            .map { $0 as! RetentionResult }
+            .scan(State.default(), accumulator: self.reducer)
+            .distinctUntilChanged()
+            .replay(1)
+        connectable.connect().disposed(by: self.disposeBag)
+        return connectable
+    }
+    private func taskObservable(resultObservable: Observable<Result>) -> Observable<Task> {
+        let connectable = resultObservable.filter { $0 is DisposableResult }
+            .map { $0 as! DisposableResult }
+            .map(self.taskFrom)
+            .publish()
+        
+        connectable.connect().disposed(by: self.disposeBag)
+        return connectable
+    }
+    
+    // MARK: - Initializer
+    public init(processor: AnyProcessor<Action, Result>) {
+        self.processor = processor
+    }
+    
+    // MARK: - Public functions
+    public func process(intents: Observable<Intent>) {
+        _ = intents.subscribe(self.intentsSubject)
+    }
+    
+    public func intentFilter() -> ComposeTransformer<Intent, Intent> {
+        fatalError()
+    }
+    
+    public func actionFrom(intent: Intent) -> Action {
+        fatalError()
+    }
+    
+    public func taskFrom(result: DisposableResult) -> Task {
+        fatalError()
+    }
+    
+    public func reducer(previousState: State, result: RetentionResult) -> State {
+        fatalError()
+    }
+}
+
 public final class AnyViewModel<I, S, T>: MviViewModelProtocol {
     
     public typealias Intent = I
