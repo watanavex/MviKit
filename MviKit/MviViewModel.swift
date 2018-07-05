6bfa6710ebedc9c627cb73b1bd6272d8438e7859
watanave <susan.se3p@gmail.com>
2018-07-05 17:50:46 +0900

    Improve ViewModelProtocol

--- MviKit/MviViewModel.swift
+++ MviKit/MviViewModel.swift
@@ -14,8 +14,8 @@ public protocol MviViewModelProtocol {
     associatedtype State
     associatedtype Task
     
-    func state() -> Observable<State>
-    func task() -> Observable<Task>
+    var state: Observable<State> { get }
+    var task: Observable<Task> { get }
     func process(intents: Observable<Intent>)
 }
 
@@ -25,23 +25,17 @@ public final class AnyViewModel<I, S, T>: MviViewModelProtocol {
     public typealias State = S
     public typealias Task = T
     
-    private let _state: ()->Observable<State>
-    private let _task: ()->Observable<Task>
+    public let state: Observable<State>
+    public let task: Observable<Task>
     private let _processIntent: (Observable<Intent>)->Void
     
-    init<Impl: MviViewModelProtocol>(_ impl: Impl) where Impl.Intent == I, Impl.State == S, Impl.Task == T {
-        self._state = impl.state
+    public init<Impl: MviViewModelProtocol>(_ impl: Impl) where Impl.Intent == I, Impl.State == S, Impl.Task == T {
+        self.state = impl.state
+        self.task = impl.task
         self._processIntent = impl.process
-        self._task = impl.task
     }
     
-    public func state() -> Observable<State> {
-        return self._state()
-    }
     public func process(intents: Observable<Intent>) {
         self._processIntent(intents)
-    }
-    public func task() -> Observable<Task> {
-        return self._task()
     }
 }
